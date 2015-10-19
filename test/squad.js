d41e93f773b006bda5f39aa6c46a0ece85508be2
coderaiser <mnemonic.enemy@gmail.com>
2015-10-19 10:23:58 -0400

    feature(squad) add

---
+++ test/squad.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+    
+    let squad    = require('..'),
+        test    = require('tape');
+    
+    let buzz = str => str + '... zzz...';
+    let scream = str => str.toUpperCase();
+    
+    test('compose functions f(g(h)', t => {
+        let noise = squad(buzz, scream);
+        
+        t.equal(noise('hello'), 'HELLO... zzz...');
+        t.end();
+    });
+    
+    test('compose functions g(f(h)', t => {
+        let noise = squad(scream, buzz);
+        
+        t.equal(noise('hello'), 'HELLO... ZZZ...');
+        t.end();
+    });
+    
+    test('no arguments', t => {
+        t.throws(squad, /fn should be function!/, 'should throw when no fn');
+        t.end();
+    });
+    
+    test('arguments: wrong type', t => {
+        let fn  = () => squad(1);
+       
+        t.throws(fn, /fn should be function!/, 'should throw when not function');
+        t.end();
+    });
+})();
