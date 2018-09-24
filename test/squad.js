1ebe486eb41b476979a467d1c21e5679d0fc14ff
coderaiser <coderaiser@cloudcmd.io>
2018-09-24 22:40:19 +0300

    feature(squad) speed up to x3 times

--- test/squad.js
+++ test/squad.js
@@ -7,6 +7,20 @@ const buzz = str => str + '... zzz...';
 const scream = str => str.toUpperCase();
 const join = (str1, str2) => str1 + str2;
 
+test('squad: no args', t => {
+    const noise = squad();
+    
+    t.notOk(noise('hello'), 'returns nothing'); 
+    t.end();
+});
+
+test('compose functions f(x)', t => {
+    const noise = squad(buzz);
+    
+    t.equal(noise('hello'), 'hello... zzz...');
+    t.end();
+});
+
 test('compose functions f(g(x)', t => {
     const noise = squad(buzz, scream);
     
@@ -21,22 +35,48 @@ test('compose functions g(f(x)', t => {
     t.end();
 });
 
+test('compose functions g(f(x): 4 fns', t => {
+    const noise = squad(scream, buzz, scream, buzz);
+    
+    t.equal(noise('hello'), 'HELLO... ZZZ...... ZZZ...');
+    t.end();
+});
+
+test('compose functions g(f(x): 5 fns', t => {
+    const inc = (a) => ++a;
+    const noise = squad(inc, inc, inc, inc, inc);
+    
+    t.equal(noise(1), 6, 'should equal');
+    t.end();
+});
+
+test('compose functions g(f(x): 6 fns', t => {
+    const inc = (a) => ++a;
+    const noise = squad(inc, inc, inc, inc, inc, inc);
+    
+    t.equal(noise(0), 6, 'should equal');
+    t.end();
+});
+
+test('compose functions g(f(x): 7 fns', t => {
+    const inc = (a) => ++a;
+    const noise = squad(inc, inc, inc, inc, inc, inc, inc);
+    
+    t.equal(noise(1), 8, 'should equal');
+    t.end();
+});
+
+test('compose functions g(f(x): 8 fns', t => {
+    const noise = squad(scream, buzz, scream, buzz, scream, buzz, scream, buzz);
+    
+    t.equal(noise('hello'), 'HELLO... ZZZ...... ZZZ...... ZZZ...... zzz...');
+    t.end();
+});
+
 test('compose functions f(g(a, b)', t => {
     const noise = squad(scream, buzz, join);
     
     t.equal(noise('hello', ' world'), 'HELLO WORLD... ZZZ...');
-    t.end();
-});
-
-test('no arguments', t => {
-    t.throws(squad, /fn should be function!/, 'should throw when no fn');
-    t.end();
-});
-
-test('arguments: wrong type', t => {
-    const fn  = () => squad(1);
-   
-    t.throws(fn, /fn should be function!/, 'should throw when not function');
     t.end();
 });
 
